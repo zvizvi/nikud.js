f5605c7d1fa281f1cfd63685abbe825f4d75602b
zvizvi <5633910@gmail.com>
2016-12-19 03:02:23 +0200

    fix bugs.

--- nikud/nikud.js
+++ nikud/nikud.js
@@ -1,17 +1,62 @@
 ﻿$(document).ready(function() {
 	jQuery.fn.extend({
 		nikud: function() {
-			$(this).each(function(i, div) {
-				new setNikud(div);
+			$(this).each(function(i, input) {
+				new setNikud(input);
 			})
 		}
 	})
-	$('.nikud').nikud();
 });
-setNikud = function(inputDiv) {
+setNikud = function(input) {
 	var self = this;
-	self.inputDiv = $(inputDiv);
-	self.keyboardHtml = '<div class="nikud-keyboard">\
+
+	self.nikudInput = $(input).wrap('<div class="nikud-wrap"></div>');
+	self.nikudDiv = self.nikudInput.closest('.nikud-wrap');
+	self.nikudInput.after(self.keyboardHtml);
+
+	self.allKeys = self.nikudDiv.find('.nikud-key');
+	self.nikudKey = self.nikudDiv.find('.nikud-key:not(#nikud-key-dagesh)');
+	self.dageshKey = self.nikudDiv.find('#nikud-key-dagesh');
+
+	self.checkChars = function() {
+		var val = self.nikudInput.val();
+		var currentChar = val.charCodeAt(self.nikudInput[0].selectionEnd - 1);
+		var secoundCurrentChar = val.charCodeAt(self.nikudInput[0].selectionEnd - 2);
+
+		if (isNaN(currentChar) || currentChar < 1488 || currentChar > 1514) {
+			self.allKeys.addClass('-disabled');
+			if (
+				currentChar == 1468 && secoundCurrentChar > 1455 && secoundCurrentChar < 1470 ||
+				secoundCurrentChar == 1468 && currentChar > 1455 && currentChar < 1470
+			) {} else if (currentChar == 1468) { //dagesh
+				self.nikudKey.removeClass('-disabled');
+			} else if (currentChar > 1455 && currentChar < 1470) { //nikud
+				self.dageshKey.removeClass('-disabled');
+			}
+		} else {
+			self.allKeys.removeClass('-disabled');
+		}
+	}
+
+	self.checkChars();
+	self.nikudInput.keyup(self.checkChars);
+	self.nikudInput.mouseup(self.checkChars);
+
+	self.nikudDiv.find('.nikud-key').click(function() {
+		if (!$(this).hasClass('-disabled')) {
+			var pos = self.nikudInput[0].selectionEnd;
+			self.nikudInput.val(
+				self.nikudInput.val().slice(0, pos) + $(this).attr('data-nikud') + self.nikudInput.val().slice(pos)
+			)
+			self.nikudInput[0].setSelectionRange(pos + 1, pos + 1)
+			self.checkChars();
+		}
+	})
+	self.nikudDiv.click(function() {
+		self.nikudInput.focus();
+	})
+}
+setNikud.prototype.keyboardHtml = '<div class="nikud-keyboard">\
 		<div class="nikud-key" id="nikud-key-kamatz" data-nikud="ָ">\
 			<span>○</span>\
 		</div>\
@@ -49,57 +94,3 @@ setNikud = function(inputDiv) {
 			<span>○</span>\
 		</div>\
 	</div>';
-
-	self.inputDiv.find('input[type=text]').after(self.keyboardHtml);
-
-	self.nikudInput = self.inputDiv.find('input');
-	self.kamatzKey = self.inputDiv.find('#nikud-key-kamatz');
-	self.patachKey = self.inputDiv.find('#nikud-key-patach');
-	self.tzereKey = self.inputDiv.find('#nikud-key-tzere');
-	self.segolKey = self.inputDiv.find('#nikud-key-segol');
-	self.hiriqKey = self.inputDiv.find('#nikud-key-hiriq');
-	self.holamKey = self.inputDiv.find('#nikud-key-holam');
-	self.kubutzKey = self.inputDiv.find('#nikud-key-kubutz');
-	self.shvaKey = self.inputDiv.find('#nikud-key-shva');
-	self.allKey = self.inputDiv.find('.nikud-key');
-	self.nikudKey = self.inputDiv.find('.nikud-key:not(#nikud-key-dagesh)');
-	self.dageshKey = self.inputDiv.find('#nikud-key-dagesh');
-
-	self.checkChars = function() {
-		var val = self.nikudInput.val();
-		var currentChar = val.charCodeAt(self.nikudInput[0].selectionEnd - 1);
-		var secoundCurrentChar = val.charCodeAt(self.nikudInput[0].selectionEnd - 2);
-
-		if (isNaN(currentChar) || currentChar < 1488 || currentChar > 1514) {
-			self.allKey.addClass('-disabled');
-			if (
-				currentChar == 1468 && secoundCurrentChar > 1455 && secoundCurrentChar < 1470 ||
-				secoundCurrentChar == 1468 && currentChar > 1455 && currentChar < 1470
-			) {} else if (currentChar == 1468) { //dagesh
-				self.nikudKey.removeClass('-disabled');
-			} else if (currentChar > 1455 && currentChar < 1470) { //nikud
-				self.dageshKey.removeClass('-disabled');
-			}
-		} else {
-			self.allKey.removeClass('-disabled');
-		}
-	}
-
-	self.checkChars();
-	self.nikudInput.keyup(self.checkChars)
-	self.nikudInput.mouseup(self.checkChars)
-
-	self.inputDiv.find('.nikud-key').click(function() {
-		if (!$(this).hasClass('-disabled')) {
-			var pos = self.nikudInput[0].selectionEnd;
-			self.nikudInput.val(
-				self.nikudInput.val().slice(0, pos) + $(this).attr('data-nikud') + self.nikudInput.val().slice(pos)
-			)
-			self.nikudInput[0].setSelectionRange(pos + 1, pos + 1)
-			self.checkChars();
-		}
-	})
-	self.inputDiv.click(function() {
-		self.nikudInput.focus();
-	})
-}
